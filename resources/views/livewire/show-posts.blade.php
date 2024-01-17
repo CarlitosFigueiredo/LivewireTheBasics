de02b403a238bcf5052e0d4157a5feb018d3f6f1
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-17 10:31:13 -0300

    lesson - 6 - Basic Table

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -9,10 +9,12 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Post title</td>
-                <td>Post content...</td>
-            </tr>
+            @foreach ($posts as $post)
+                <tr>
+                    <td>Post title</td>
+                    <td>Post content...</td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
