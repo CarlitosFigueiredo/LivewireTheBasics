41b6daf926975537dbc8c529f7a34be2db54aa41
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-17 13:26:58 -0300

    lesson - 10 - Nesting

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,4 +19,10 @@ class Post extends Model
         'content',
         'is_archived',
     ];
+
+    public function archive()
+    {
+        $this->is_archived = true;
+        $this->save();
+    }
 }
